705b31b8811484ef6db863007d0cb9c8d17ae469
Gustavo Rodrigues <gustavohrx123@gmail.com>
2024-01-31 14:03:13 -0300

    O site em si

---
+++ src/js/scripts.js
@@ -0,0 +1,8 @@
+//Objetivo 1
+const botao = document.querySelector(".btn-plataforma")
+
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas")
+
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+})
